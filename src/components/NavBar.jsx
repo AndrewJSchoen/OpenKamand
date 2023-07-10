077b53c6933647f7ad47214256548e1b02d99316
Andrew Schoen <schoen.andrewj@gmail.com>
2023-07-10 09:00:06 -0500

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -170,7 +170,6 @@ export const NavBar = () => {
           </Box>
         </Toolbar>
       </Container>
-      <Settings open={settingsOpen} onClose={()=>setSettingsOpen(false)}/>
     </AppBar>
   );
 };
